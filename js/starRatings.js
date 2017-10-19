f86a2d38b1abd161398d250b559a70a7ef9e0f17
JavVogt <jav.vogt@gmail.com>
2017-10-20 01:23:06 +1030

    Add files via upload

---
+++ js/starRatings.js
@@ -0,0 +1,9 @@
+function starRatingBuilding() {
+    var pic = "images/4star.png"
+    document.getElementById('buildingStar').src = pic;
+}
+
+function starRatingReset() {
+    var pic = "images/0star.png"
+    document.getElementById('buildingStar').src = pic;
+}
