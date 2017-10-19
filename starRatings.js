6828326d440dc363d7203193d1a5e3a2b31ad44f
JavVogt <jav.vogt@gmail.com>
2017-10-20 01:22:32 +1030

    Delete starRatings.js

--- starRatings.js
+++
@@ -1,9 +0,0 @@
-function starRatingBuilding() {
-    var pic = "images/4star.png"
-    document.getElementById('buildingStar').src = pic;
-}
-
-function starRatingReset() {
-    var pic = "images/0star.png"
-    document.getElementById('buildingStar').src = pic;
-}
